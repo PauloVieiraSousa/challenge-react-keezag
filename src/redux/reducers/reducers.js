e72b7e011689a9db60034c9a3f62f945840ae452
sousapaulo <sousapaulo@localhost.localdomain>
2020-06-29 14:45:44 -0300

    EsLint added to standardize the code according to the airbnb style guide
    issue: #3

--- src/redux/reducers/reducers.js
+++ src/redux/reducers/reducers.js
@@ -1,9 +1,8 @@
- 
 import { combineReducers } from 'redux';
 import example from './example.reducer';
 
 const reducers = combineReducers({
-    example: example,
+  example
 });
 
 export default reducers;
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers/reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__  || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
   reducers,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
-)
+  composeEnhancers(applyMiddleware(thunk))
+);
