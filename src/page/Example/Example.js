980d3287fae597c63674a39fd2d3c3d21145700d
sousapaulo <sousapaulo@localhost.localdomain>
2020-06-29 15:28:49 -0300

    Added Framework Material-Ui
    Added Material-Ui structure with component user interface
    
    issue: #6

--- src/page/Example/Example.js
+++ src/page/Example/Example.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import Icon from '@material-ui/core/Icon';
 
-const Example = () => <h1>Example</h1>;
+const Example = () => (
+  <>
+    <h1>Example</h1>
+    <Icon color="primary">add_circle</Icon>
+  </>
+);
 
 export default Example;
